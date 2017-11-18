15ec9047997637615feb460e3f415ad05749c62d
Tracey <tracey.wilhelmsen@gmail.com>
2017-11-18 04:22:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+//const employees = ['Ada', 'Brendan', 'Ali'];
+
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
+  }
+  return employees;
+}
+
+function coinflip() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let counter = 0;
+  while (coinflip()) {
+    counter++
+  }
+
+  return `You got ${counter} tails in a row!`
+}
