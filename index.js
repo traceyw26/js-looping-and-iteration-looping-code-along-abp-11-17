d7000d9f82d6c61330b3792d735b38fde4790820
Tracey <tracey.wilhelmsen@gmail.com>
2017-11-18 02:52:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+const employees = ['Ada', 'Brendan', 'Ali'];
+
+function printBadges(employee) {
+  for (let i = 0, i < employees.length, i++) {
+    console.log(`Welcome employees.[i]! You are employee #`)
+  }
+}
